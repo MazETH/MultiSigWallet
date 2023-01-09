71b026f26ae705f4a34a15b46518dcdb604c1851
Abdullah Mazhar <a.mazhar0098@gmail.com>
2023-01-09 18:02:43 -0500

    testing

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
@@ -25,6 +25,27 @@ describe("Multi Sig Wallet Contract", () => {
         amountSend,
         "0x"
       );
+      let transactionId = await multiSigWallet.transactions("0");
+      let transactionValue = transactionId.value;
+      await expect(transactionValue).to.equal(amountSend);
     });
+  });
+});
+
+describe("signTransaction", () => {
+  it("Signs a pending transaction", async () => {
+    let initialSingerAddress = signer2.address;
+    let amountSend = "100000000000000000";
+    let initialTransaction = await multiSigWallet.initiateTransaction(
+      initialSingerAddress,
+      amountSend,
+      "0x"
+    );
+    let transactionId = "0";
+
+    await multiSigWallet.connect(signer3).signTransaction(transactionId);
+    let transaction = await multiSigWallet.transactions(transactionId);
+    let numberOfTransactionSigners = transaction.confirmations;
+    await expect(numberOfTransactionSigners).to.equal("1");
   });
 });
